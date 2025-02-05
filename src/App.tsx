a089d8a8136dc6683e447876819382345b0c671b
Ryan Zheng <rzpersonal1@gmail.com>
2025-02-05 16:07:55 -0600

    added capability to switch between pomo, short break, long break mode

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,63 @@ import './App.css'
 
 function App() {
 
-  function testOnClick(): void {
-    console.log('yes');
+  const pomoSeconds: number = 1500;
+  const shortBreakSeconds: number = 300; 
+  const longBreakSeconds: number = 900; 
+
+  const timerDisplay = document.getElementById('timer-text');
+
+  //state
+  const [currRotation, setCurrRotation] = useState(pomoSeconds);
+  const [isRunning, setIsRunning] = useState(false);
+
+  function displayTime(seconds: number) {
+      const timeLeft = seconds;
+      setCurrRotation(seconds);
+      if (timerDisplay) {
+        timerDisplay.textContent = ((timeLeft/60) >= 10) ? ((timeLeft/60).toString() + ":00") : ("0" + (timeLeft/60).toString() + ":00");
+      }
+      setCurrRotation(seconds)
+  }
+
+  function startTimer(current: number) {
+      setIsRunning(prev => !prev);
+      let timeLeft = current;
+
+      function update() {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+
+        if(timerDisplay) {
+          timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2,'0')}`;
+        }
+
+        if(timeLeft <= 0) {
+          clearInterval(countdown);
+        } else {
+          timeLeft--;
+        }
+      }
+      update();
+      const countdown = setInterval(update, 1000);
   }
 
   return (
     <>
       <main>
-        <div className="timer" onClick={testOnClick}>
-          
-          <h1 className="timer-text">25:00</h1>
-          <p className="timer-start-text">click to start</p>
-          
+        <div className="timer" onClick={() => startTimer(currRotation)}>
+          <h1 id="timer-text">25:00</h1>
+          <p id="timer-start-text">{isRunning ? 'click to pause' : 'click to start'}</p>
         </div>
         <div className ="timer-buttons">
-            <button id="pomodoro" onClick={testOnClick}>pomodoro</button>
-            <button id="short-break" onClick={testOnClick}>short break</button>
-            <button id="long-break" onClick={testOnClick}>long break</button>
+            <button id="pomodoro" onClick={() => displayTime(pomoSeconds)}>pomodoro</button>
+            <button id="short-break" onClick={() => displayTime(shortBreakSeconds)}>short break</button>
+            <button id="long-break" onClick={() => displayTime(longBreakSeconds)}>long break</button>
         </div>
         <div className="misc-buttons">
-            <img id="setting" src={setting}  onClick={testOnClick}/>
-            <img id="reset" src={reset} onClick={testOnClick}/>
-            <img id="chart" src={chart} onClick={testOnClick}/>
+            <img id="setting" src={setting}/>
+            <img id="reset" src={reset}/>
+            <img id="chart" src={chart} />
         </div>
       </main>
     </>
