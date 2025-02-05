1a15ff763384963a7fa5f4df8b54e4a16f61e5df
Ryan Zheng <rzpersonal1@gmail.com>
2025-02-04 22:34:09 -0600

    added center timer + timer start text (all styled)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import ellipse from './assets/ellipse-button.png'
 
 import './App.css'
 
@@ -7,7 +8,15 @@ function App() {
   return (
     <>
       <main>
-        <h1>25:00</h1>
+        <div className="timer">
+          <h1 className="timer-text">25:00</h1>
+          <p className="timer-start-text">click to start</p>
+        </div>
+        <div className ="timer-buttons">
+            <button id="pomodoro">pomodoro</button>
+            <button id="short-break">short break</button>
+            <button id="long-break">long break</button>
+        </div>
       </main>
     </>
   )
