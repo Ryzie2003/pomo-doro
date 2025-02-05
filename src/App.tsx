f2ffc50e3660d698b84565d7f62445d35da3d078
Ryan Zheng <rzpersonal1@gmail.com>
2025-02-04 23:00:43 -0600

    added pomo, 2 break buttons, setting, reset, chart buttons - used css grid to apply spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import ellipse from './assets/ellipse-button.png'
+import reset from './assets/reset.png'
+import setting from './assets/setting.png'
+import chart from './assets/bar-chart.png'
 
 import './App.css'
 
@@ -9,13 +12,20 @@ function App() {
     <>
       <main>
         <div className="timer">
+          
           <h1 className="timer-text">25:00</h1>
           <p className="timer-start-text">click to start</p>
+          
         </div>
         <div className ="timer-buttons">
             <button id="pomodoro">pomodoro</button>
             <button id="short-break">short break</button>
             <button id="long-break">long break</button>
+        </div>
+        <div className="misc-buttons">
+            <img id="setting" src={setting}/>
+            <img id="reset" src={reset}/>
+            <img id="chart" src={chart}/>
         </div>
       </main>
     </>
