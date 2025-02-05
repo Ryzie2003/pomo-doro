e1576bc8a7493a097ae62b01f937b6709aafe86d
Ryan Zheng <rzpersonal1@gmail.com>
2025-02-04 23:06:41 -0600

    added onClick functionality and cursor: pointer to all buttons

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,28 @@ import './App.css'
 
 function App() {
 
+  function testOnClick(): void {
+    console.log('yes');
+  }
+
   return (
     <>
       <main>
-        <div className="timer">
+        <div className="timer" onClick={testOnClick}>
           
           <h1 className="timer-text">25:00</h1>
           <p className="timer-start-text">click to start</p>
           
         </div>
         <div className ="timer-buttons">
-            <button id="pomodoro">pomodoro</button>
-            <button id="short-break">short break</button>
-            <button id="long-break">long break</button>
+            <button id="pomodoro" onClick={testOnClick}>pomodoro</button>
+            <button id="short-break" onClick={testOnClick}>short break</button>
+            <button id="long-break" onClick={testOnClick}>long break</button>
         </div>
         <div className="misc-buttons">
-            <img id="setting" src={setting}/>
-            <img id="reset" src={reset}/>
-            <img id="chart" src={chart}/>
+            <img id="setting" src={setting}  onClick={testOnClick}/>
+            <img id="reset" src={reset} onClick={testOnClick}/>
+            <img id="chart" src={chart} onClick={testOnClick}/>
         </div>
       </main>
     </>
