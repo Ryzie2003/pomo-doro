7ead043158c653cfffbb939545f7f2a4dd7d9465
Ryan Zheng <rzpersonal1@gmail.com>
2025-02-13 22:07:28 -0600

    Initial REST API created

--- backend/src/controllers/timerController.ts
+++ backend/src/controllers/timerController.ts
@@ -1,7 +1,10 @@
 import asyncHandler from 'express-async-handler';
+import { userModel } from '../models/userModel';
 
 export const getTimers = asyncHandler(async (req, res) => {
-    res.status(200).json({ msg: "read timers"});
+    const timers = await userModel.find();
+
+    res.status(200).json(timers);
 })
 
 export const getSingleTimer = asyncHandler(async (req, res) => {
@@ -9,17 +12,43 @@ export const getSingleTimer = asyncHandler(async (req, res) => {
 })
 
 export const createTimer = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
+    const createUser = await userModel.create({
+        username: req.body.username,
+        password: req.body.password,
+        weeklyArray: [1, 2, 3],
+    })
+    if (!req.body.username) {
         res.status(400);
         throw new Error(`Please add a text field.`);
     }
-    res.status(201).json({ msg: `timer created`});
+    res.status(201).json(createUser);
 })
 
 export const updateTimer = asyncHandler(async (req, res) => {
-    res.status(201).json({ msg: `timer with id ${req.params.id} updated. `});
+    // find the user with req.params.id
+    const user = await userModel.findById(req.params.id);
+
+    // error check
+    if (user) {
+        const updatedUser = await userModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(201).json(updatedUser);
+    } else {
+        res.status(400);
+        throw new Error(`Could not find user with ${req.params.id}`)
+    }
+
+    // update the user with req.params.id
 })
 
 export const deleteTimer = asyncHandler(async (req, res) => {
-    res.status(201).json({ msg: `timer with id ${req.params.id} deleted. `});
+    const user = await userModel.findById(req.params.id);
+
+    // error check
+    if (user) {
+        const deletedUser = await userModel.findByIdAndDelete(req.params.id)
+        res.status(201).json({msg: `Deleted user: ${deletedUser}`});
+    } else {
+        res.status(400);
+        throw new Error(`Could not find user with ${req.params.id}`)
+    }
 })
